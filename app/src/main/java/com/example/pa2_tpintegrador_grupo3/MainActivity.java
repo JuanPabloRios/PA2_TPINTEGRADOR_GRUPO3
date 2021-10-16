f9b7f80c8695edd2a108af0005ea1cd0ceb38a18
Juan Pablo Rios <jp@arpedio.com>
2021-10-16 17:01:26 +0200

    Mas pantallas

--- app/src/main/java/com/example/pa2_tpintegrador_grupo3/MainActivity.java
+++ app/src/main/java/com/example/pa2_tpintegrador_grupo3/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.pa2_tpintegrador_grupo3;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.dispositivos_vinculados);
     }
 }
