1a1ac5fd17cba03df45af5f323096220e9acc098
NorevDurok <81711551+NorevDurok@users.noreply.github.com>
2021-11-05 20:52:12 -0300

    Getters y Setters Aplicacion
    y agregue los getters y setters para la clase Aplicacion, espero que lo puedan mirar pronto.

--- app/src/main/java/com/example/pa2_tpintegrador_grupo3/entidades/Aplicacion.java
+++ app/src/main/java/com/example/pa2_tpintegrador_grupo3/entidades/Aplicacion.java
@@ -12,4 +12,34 @@ public class Aplicacion
 		Descripcion = descripcion;
 		Eliminado = eliminado;
 	}
+
+	public Integer getId()
+	{
+		return Id;
+	}
+
+	public void setId(Integer id)
+	{
+		Id = id;
+	}
+
+	public String getDescripcion()
+	{
+		return Descripcion;
+	}
+
+	public void setDescripcion(String descripcion)
+	{
+		Descripcion = descripcion;
+	}
+
+	public boolean isEliminado()
+	{
+		return Eliminado;
+	}
+
+	public void setEliminado(boolean eliminado)
+	{
+		Eliminado = eliminado;
+	}
 }
