b216176b45f842614e37a97f64a6e39379901ef7
Ezequiel Rios <ezequiel.rios@kavak.com>
2021-11-06 15:55:32 -0300

    Entidad TipoDispositivo

---
+++ app/src/main/java/com/example/pa2_tpintegrador_grupo3/entidades/TipoDispositivo.java
@@ -0,0 +1,35 @@
+package com.example.pa2_tpintegrador_grupo3.entidades;
+
+public class TipoDispositivo {
+    private Integer id;
+    private String descripcion;
+
+    public TipoDispositivo(Integer id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoDispositivo{" +
+                "id=" + id +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
