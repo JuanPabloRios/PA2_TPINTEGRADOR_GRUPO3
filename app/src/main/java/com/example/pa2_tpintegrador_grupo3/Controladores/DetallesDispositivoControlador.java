80cb6967bd49ee7b6d79c8e853eaaabd77f182ff
Ezequiel Rios <ezequiel.rios@kavak.com>
2021-11-08 22:24:00 -0300

    Modificaciones en Controladores

--- app/src/main/java/com/example/pa2_tpintegrador_grupo3/Controladores/DetallesDispositivoControlador.java
+++ app/src/main/java/com/example/pa2_tpintegrador_grupo3/Controladores/DetallesDispositivoControlador.java
@@ -1,2 +1,15 @@
-package com.example.pa2_tpintegrador_grupo3.Controladores;public class DetallesDispositivoControlador {
+package com.example.pa2_tpintegrador_grupo3.Controladores;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pa2_tpintegrador_grupo3.R;
+
+public class DetallesDispositivoControlador  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detalles_dispositivo);
+    }
 }
--- app/src/main/java/com/example/pa2_tpintegrador_grupo3/Controladores/PrincipalSubordinadoControlador.java
+++ app/src/main/java/com/example/pa2_tpintegrador_grupo3/Controladores/PrincipalSubordinadoControlador.java
@@ -1,2 +1,18 @@
-package com.example.pa2_tpintegrador_grupo3.Controladores;public class PrincipaSubordinadoControlador {
+package com.example.pa2_tpintegrador_grupo3.Controladores;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pa2_tpintegrador_grupo3.R;
+
+public class PrincipalSubordinadoControlador  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_subordinado);
+
+    }
 }
